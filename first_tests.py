68f51b75d522be4b9e7912f98681b03812c71ebb
noamfree <noam.freeman@mail.huji.ac.il>
2017-09-15 06:00:27 +0300

    manually confirm running a method

--- first_tests.py
+++ first_tests.py
@@ -3,8 +3,18 @@ written by noam.freeman
 """
 
 # manually confirm running a method
+class WasRun:
+    def __init__(self):
+        self.was_run = None
+
+    def testing_method(self):
+        self.was_run = True
+
+
 def test_was_run():
     test = WasRun()
     print(test.was_run)  # should be None
-    test.testing_method
+    test.testing_method()
     print(test.was_run)  # should be True
+
+test_was_run()
