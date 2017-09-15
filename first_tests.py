1024e0879695269245452c16cc2ebfec43ca714c
noamfree <noam.freeman@mail.huji.ac.il>
2017-09-15 06:22:24 +0300

    added teardown.

--- first_tests.py
+++ first_tests.py
@@ -6,32 +6,29 @@ from test_framework import *
 
 class WasRun(TestCase):
     def __init__(self, what_to_run):
+        self.log = ""
         self.was_setup = None
         self.was_run = None
         TestCase.__init__(self, what_to_run)
 
     def testing_method(self):
-
+        self.log += "running: testing_method "
         self.was_run = True
 
     def setup(self):
+        self.log += "setup "
         self.was_setup = True
+
+    def tear_down(self):
+        self.log += "teardown "
 
 
 class TestingTestCase(TestCase):
-
-
-    def test_was_run(self):
+    def test_setup_run_teardown_order(self):
         test = WasRun("testing_method")
-        assert test.was_run is None
+        assert test.log == ""
         test.run()
-        assert test.was_run is True
+        assert test.log == "setup running: testing_method teardown "
 
-    def test_was_setup(self):
-        test = WasRun("testing_method")
-        assert test.was_setup == None
-        test.run()
-        assert  test.was_setup == True
 
-TestingTestCase("test_was_run").run()
-TestingTestCase("test_was_setup").run()
+TestingTestCase("test_setup_run_teardown_order").run()
--- test_framework.py
+++ test_framework.py
@@ -5,6 +5,10 @@ class TestCase:
     def run(self):
         self.setup()
         exec("self." + self.what_to_run + "()")
+        self.tear_down()
 
     def setup(self):
+        pass
+
+    def tear_down(self):
         pass
